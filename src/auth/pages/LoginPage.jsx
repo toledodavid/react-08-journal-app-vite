e6228c0c77234cd3a75d1c97620929f3671dab03
David Toledo <dtoledoz0207@gmail.com>
2023-07-25 13:40:39 -0600

    Continue with design for LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from '@mui/material';
+import {Link as RouterLink} from 'react-router-dom';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 
 
 export const LoginPage = () => {
@@ -26,6 +28,26 @@ export const LoginPage = () => {
               <Grid item xs={12} sx={{mt: 2}}>
                 <TextField label="Password" type="password" fullWidth />
               </Grid>
+
+              <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
+                <Grid item xs={12} sm={6}>
+                  <Button variant='contained' fullWidth>Login</Button>
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <Button variant='contained' fullWidth>
+                    <Google />
+                    <Typography sx={{ml: 1}}>Google</Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <Link component={RouterLink} color='inherit' to="/auth/register">
+                  Create an account
+                </Link>
+              </Grid>
+
             </Grid>
           </form>
       </Grid>
