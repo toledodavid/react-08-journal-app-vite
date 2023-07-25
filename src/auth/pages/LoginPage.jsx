d9ea517ec3a124b49825aef71a4c394301271023
David Toledo <dtoledoz0207@gmail.com>
2023-07-25 13:08:00 -0600

    Starting with first design for LoginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,35 @@
+import { Grid, TextField, Typography } from '@mui/material';
 
 
 export const LoginPage = () => {
   return(
-    <h1>LoginPage</h1>
+    <Grid container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{minHeight: '100vh', backgroundColor: 'primary.main', padding: 4}}>
+
+      <Grid item
+            className='box-shadow'
+            xs={3}
+            sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}>
+
+          <Typography variant='h5' sx={{mb: 1}}>Login</Typography>
+
+          <form>
+            <Grid container>
+              <Grid item xs={12} sx={{mt: 2}}>
+                <TextField label="Email" type="email" placeholder='test@gmail.com' fullWidth />
+              </Grid>
+
+              <Grid item xs={12} sx={{mt: 2}}>
+                <TextField label="Password" type="password" fullWidth />
+              </Grid>
+            </Grid>
+          </form>
+      </Grid>
+
+    </Grid>
   );
 }
