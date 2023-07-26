666c0473c6d9f2957cf78ed9aa9f61732dae4f0a
David Toledo <dtoledoz0207@gmail.com>
2023-07-26 12:57:24 -0600

    Create JournalLayout and implement it in JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
-import { Typography } from "@mui/material";
+import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return(
-    <Typography variant="h1">JournalPage</Typography>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas repudiandae voluptates adipisci facere dolore in nulla officia asperiores illum quisquam doloremque vel debitis at ratione, deleniti temporibus quibusdam veritatis quas?</Typography>
+
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   );
 }
