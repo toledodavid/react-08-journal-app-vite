80f4be5716cff6a74974e2ac2b5103edbd80b77c
David Toledo <dtoledoz0207@gmail.com>
2023-07-31 12:54:45 -0600

    Create NothingSelectedView component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,10 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return(
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas repudiandae voluptates adipisci facere dolore in nulla officia asperiores illum quisquam doloremque vel debitis at ratione, deleniti temporibus quibusdam veritatis quas?</Typography>
-
-      {/* NothingSelected */}
+      <NothingSelectedView />
       {/* NoteView */}
     </JournalLayout>
   );
