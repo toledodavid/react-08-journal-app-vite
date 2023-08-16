197046d347957b618d301d164f952ad82f503c9f
David Toledo <dtoledoz0207@gmail.com>
2023-08-16 13:03:19 -0600

    Create thunks for auth to handle auth async actions for store

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,30 +1,53 @@
 import {Link as RouterLink} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  const {email, password, onInputChange} = useForm({
+    email: 'david@gmail.com',
+    password: '123456'
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({email, password});
+    dispatch(checkingAuthentication(email, password));
+  }
+
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+    dispatch(startGoogleSignIn());
+  }
+
   return(
 
     <AuthLayout title='Login'>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{mt: 2}}>
-            <TextField label="Email" type="email" placeholder='test@gmail.com' fullWidth />
+            <TextField label="Email" type="email" placeholder='test@gmail.com' fullWidth name="email" value={email} onChange={onInputChange} />
           </Grid>
 
           <Grid item xs={12} sx={{mt: 2}}>
-            <TextField label="Password" type="password" fullWidth />
+            <TextField label="Password" type="password" fullWidth name="password" value={password} onChange={onInputChange} />
           </Grid>
 
           <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>Login</Button>
+              <Button type='submit' variant='contained' fullWidth>Login</Button>
             </Grid>
 
             <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>
+              <Button variant='contained' fullWidth onClick={onGoogleSignIn}>
                 <Google />
                 <Typography sx={{ml: 1}}>Google</Typography>
               </Button>
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    status: 'checking', // 'not-authenticated', 'authenticated'
+    status: 'not-authenticated', // 'checking', 'not-authenticated', 'authenticated'
     uid: null,
     email: null,
     displayName: null,
@@ -19,7 +19,7 @@ export const authSlice = createSlice({
 
     },
     checkingCredentials: (state) => {
-
+      state.status = 'checking';
     }
   }
 });
