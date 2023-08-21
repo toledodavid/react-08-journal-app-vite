8defda6aa5255a30460ca9704720835444b6d1f4
David Toledo <dtoledoz0207@gmail.com>
2023-08-21 11:42:01 -0600

    Implement custom hook useForm in RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,29 +1,43 @@
 import {Link as RouterLink} from 'react-router-dom';
-import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
 
+
+const formData = {
+  displayName: 'Juan',
+  email: 'juan@gmail.com',
+  password: '123456'
+};
 
 export const RegisterPage = () => {
+
+  const {displayName, email, password, onInputChange, formState} = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  }
+
   return(
     <AuthLayout title='Create an account'>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{mt: 2}}>
-            <TextField label="Name" type="text" placeholder='Name' fullWidth />
+            <TextField label="Name" type="text" placeholder='Name' name='displayName' value={displayName} onChange={onInputChange} fullWidth />
           </Grid>
 
           <Grid item xs={12} sx={{mt: 2}}>
-            <TextField label="Email" type="email" placeholder='test@gmail.com' fullWidth />
+            <TextField label="Email" type="email" placeholder='test@gmail.com' name='email' value={email} onChange={onInputChange} fullWidth />
           </Grid>
 
           <Grid item xs={12} sx={{mt: 2}}>
-            <TextField label="Password" type="password" fullWidth />
+            <TextField label="Password" type="password" name='password' value={password} onChange={onInputChange} fullWidth />
           </Grid>
 
           <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
             <Grid item xs={12}>
-              <Button variant='contained' fullWidth>Create</Button>
+              <Button type='submit' variant='contained' fullWidth>Create</Button>
             </Grid>
           </Grid>
 
