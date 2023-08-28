e964b38aadc878a666648fbc057046b6368b35ee
David Toledo <dtoledoz0207@gmail.com>
2023-08-28 12:05:33 -0600

    Create CheckingAuth component that implements an CircularProgress and use it in AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
+import { useSelector } from 'react-redux';
+import { CheckingAuth } from '../ui';
 
 
 export const AppRouter = () => {
+
+  const {status} = useSelector(state => state.auth);
+
+  if (status === 'checking') {
+    return <CheckingAuth />;
+  }
+
   return(
     <Routes>
 
