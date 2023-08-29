a0955ccb298b4797404dc480faf93d9303d7360c
David Toledo <dtoledoz0207@gmail.com>
2023-08-29 11:38:05 -0600

    Create useCheckAuth custom hook to handle logic for checking current user authentication from firebase and clear AppRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,14 @@
-import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { onAuthStateChanged } from 'firebase/auth';
-import { FirebaseAuth } from '../firebase/config';
-
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 import { CheckingAuth } from '../ui';
-import { login, logout } from '../store/auth';
+import { useCheckAuth } from '../hooks';
 
 
 
 export const AppRouter = () => {
 
-  const {status} = useSelector(state => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-
-      const {uid, displayName, email, photoURL} = user;
-      dispatch(login({uid, displayName, email, photoURL}));
-    });
-  }, []);
+  const status = useCheckAuth();
 
   if (status === 'checking') {
     return <CheckingAuth />;
