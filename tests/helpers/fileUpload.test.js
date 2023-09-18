b9412fa3e6469f291568ebe87121d3c19dd61ffe
David Toledo <dtoledoz0207@gmail.com>
2023-09-18 12:56:56 -0600

    Add Cloudinary SDK to delete image after testing in fileUpload test file

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,13 @@
+import {v2 as cloudinary} from 'cloudinary';
 import { fileUpload } from '../../src/helpers/fileUpload';
+
+
+cloudinary.config({
+  cloud_name: 'doilfq77p',
+  api_key: '495148786141616',
+  api_secret: 'TOYYMatmYFppUTo-zir6tY4kkEk',
+  secure: true
+});
 
 
 describe('Tests for fileUpload', () => {
@@ -13,6 +22,18 @@ describe('Tests for fileUpload', () => {
     const url = await fileUpload(file);
 
     expect(typeof url).toBe('string');
+
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.png', '');
+    await cloudinary.api.delete_resources(['journal/' + imageId], {resource_type: 'image'});
+  });
+
+  test('should return null', async () => {
+    const file = new File([], 'photo.jpg');
+
+    const url = await fileUpload(file);
+
+    expect(url).toBe(null);
   });
 
 });
