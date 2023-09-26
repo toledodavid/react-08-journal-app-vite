86967ef6d87243eb25b6e46b1ac2e3d3ac2914f8
David Toledo <dtoledoz0207@gmail.com>
2023-09-26 13:42:59 -0600

    Add tests for authSlice

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,5 +1,5 @@
-import { authSlice } from '../../../src/store/auth/authSlice';
-import { initialState } from '../../fixtures/authFixtures';
+import { authSlice, checkingCredentials, login, logout } from '../../../src/store/auth/authSlice';
+import { authenticatedState, demoUser, initialState } from '../../fixtures/authFixtures';
 
 
 describe('Tests for authSlice', () => {
@@ -9,4 +9,48 @@ describe('Tests for authSlice', () => {
     expect(authSlice.name).toBe('auth');
     expect(state).toEqual(initialState);
   });
+
+  test('should do authentication', () => {
+    const state = authSlice.reducer(initialState, login(demoUser));
+    expect(state).toEqual({
+      status: 'authenticated',
+      uid: demoUser.uid,
+      email: demoUser.email,
+      displayName: demoUser.displayName,
+      photoURL: demoUser.photoURL,
+      errorMessage: null
+    });
+  });
+
+  test('should do logout without arguments', () => {
+    const state = authSlice.reducer(authenticatedState, logout());
+    expect(state).toEqual({
+      status: 'not-authenticated',
+      uid: null,
+      email: null,
+      displayName: null,
+      photoURL: null,
+      errorMessage: undefined
+    });
+  });
+
+  test('should do logout with arguments', () => {
+    const errorMessage = 'Invalid credentials';
+
+    const state = authSlice.reducer(authenticatedState, logout(errorMessage));
+    expect(state).toEqual({
+      status: 'not-authenticated',
+      uid: null,
+      email: null,
+      displayName: null,
+      photoURL: null,
+      errorMessage: errorMessage
+    });
+  });
+
+  test('should change the state to checking', () => {
+    const state =  authSlice.reducer(authenticatedState, checkingCredentials());
+    expect(state.status).toBe('checking');
+  });
+
 });
